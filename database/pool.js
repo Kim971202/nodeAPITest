cdb734463ac4b145365edf4a4fbdaa6a928af022
Kim971202 <kimyohan971202@gmail.com>
2022-08-16 21:07:48 +0900

    ubuntu test

--- database/pool.js
+++ database/pool.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2/promise");
 
-//VSCODE TEST
 const pool = mysql.createPool({
   host: "43.200.134.34",
   port: "3306",
   user: "root",
-  password: "1234",
+  password: "testPWD",
   database: "complexdb",
   connectionLimit: 10,
 });
