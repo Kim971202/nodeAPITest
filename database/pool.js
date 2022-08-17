ac7f020561fd181fc0cb3155d8eee16de7e68018
Kim971202 <kimyohan971202@gmail.com>
2022-08-18 08:14:45 +0900

    host change local -> cloud

--- database/pool.js
+++ database/pool.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 // localhost - for local test
 // 43.200.134.34 - for cloud test
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "43.200.134.34",
   port: "3306",
   user: "root",
   password: "1234",
